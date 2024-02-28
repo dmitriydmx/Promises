a3a948cb1c374bfadaf620b6a00d7070ee2adb19
dmitriydmx <dmitriydmx@mail.ru>
2024-02-28 18:31:50 +0900

    fixed promises

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,15 +1,15 @@
-import read from './reader.js';
 import json from './parser.js';
+import read from './reader.js';
+import GameSaving from './GameSaving.js';
 
 export default class GameSavingLoader {
   static load() {
     return new Promise((resolve, reject) => {
       read()
-        .then((data) => {
-          return json(data);
-        })
-        .then((value) => {
-          resolve(JSON.parse(value));
+        .then((data) => json(data))
+        .then((parsedData) => {
+          const saving = new GameSaving(JSON.parse(parsedData));
+          resolve(saving);
         })
         .catch((error) => {
           reject(error);
